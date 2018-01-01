75efac20558ebdf47c4b10b94f6aa1d79d0db705
Lore Palominos <lorena.palominos@gmail.com>
2018-01-01 18:40:41 -0300

    añadiendo seccion sign up

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
   $('#status').delay(3000).fadeOut('slow');
   $('#preloader').delay(3000).fadeOut();
   $('.second-screen').hide();
+  $('#sign-up').hide();
   
   /*
   *Tiempo en que demora en aparecer la segunda pantalla
@@ -15,6 +16,18 @@ $(document).ready(function(){
   setTimeout(function(){
   $('.second-screen').show();
   }, 3000);
+
+ /*
+  *Evento de click que muestra el botón y esconde mi segunda pantalla
+  */
+  $('.sign-up').click(function(){
+    $('#sign-up').show();
+    $('.second-screen').hide();
+  });
+  $('.sign-up').click(function(){
+
+  });
+
 
 
 }); //Fin de la función principal.
